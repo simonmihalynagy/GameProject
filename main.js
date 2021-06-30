ad1bacea40fb6482d80527d2e9ae435ccd4f92fc
Simon Nagy <nagysimonmihaly@gmail.com>
2021-06-30 08:03:36 +0200

    add enemy object

--- main.js
+++ main.js
@@ -5,6 +5,56 @@ const directions = ["Up", "Down", "Left", "Right"];
 
 let shapes = [];
 
+class Ball {
+  constructor() {
+    this.x = null;
+    this.y = null;
+    this.width = 20;
+    this.height = 20;
+    this.xSpeed = 1;
+    this.ySpeed = 1;
+  }
+  _pickRandomStart() {
+    this.x = Math.floor(Math.random() * (790 - 10) + 1);
+    this.y = this.x;
+  }
+  _move() {
+    if (this.x + this.xSpeed > canvas.width - 20 || this.x + this.xSpeed < 0) {
+      this.xSpeed = -this.xSpeed;
+    }
+    if (this.y + this.ySpeed > canvas.height - 20 || this.y + this.ySpeed < 0) {
+      this.ySpeed = -this.ySpeed;
+    }
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+  }
+  _draw() {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+//check if ball touches line
+let intersectWithLine = (pen, ball) => {
+  //line started on the left
+  if (pen.direction === "left") {
+    //single line
+  }
+};
+//check if ball collides with drawn shapes
+let intersectShapes = (ball, shapes) => {
+  //loop through shapes
+  shapes.forEach((shape) => {
+    //check for shapes with 1 drawn line
+    if (Object.keys(shape).length === 4) {
+      //line started from left, in the upper half of the Canvas
+      if (shape.startPosX === 0 && shape.startPosY < canvas.height / 2) {
+        if (ball.y < shape.startPosY) {
+          ball.ySpeed = -ball.ySpeed;
+        }
+      }
+    }
+  });
+};
+
 let drawShapes = function (shapes) {
   shapes.forEach((shape) => {
     if (Object.keys(shape).length === 4) {
@@ -301,238 +351,12 @@ let drawShapes = function (shapes) {
   });
 };
 
-
-let ball = {
-  startX : 
-  startY : ball.startX
-}
-
-class Pen {
-  constructor() {
-    this.direction = directions[Math.floor(Math.random() * directions.length)];
-
-    this.startPosX = null;
-    this.startPosY = null;
-
-    this.firstCornerX = null;
-    this.firstCornerY = null;
-
-    this.secondCornerX = null;
-    this.secondCornerY = null;
-
-    this.lastCornerX = null;
-    this.lastCornerY = null;
-
-    this.turnCounter = null;
-  }
-
-  pickRandomStart() {
-    if (this.direction === "Up") {
-      this.startPosX = Math.floor(Math.random() * (790 - 10) + 1);
-      this.startPosY = canvas.height;
-      this.firstCornerX = this.startPosX;
-      this.firstCornerY = this.startPosY;
-      this.secondCornerX = this.startPosX;
-      this.secondCornerY = this.startPosY;
-      this.lastCornerX = this.startPosX;
-      this.lastCornerY = this.startPosY;
-    } else if (this.direction === "Down") {
-      this.startPosX = Math.floor(Math.random() * (790 - 10) + 1);
-      this.startPosY = 0;
-      this.firstCornerX = this.startPosX;
-      this.firstCornerY = this.startPosY;
-      this.secondCornerX = this.startPosX;
-      this.secondCornerY = this.startPosY;
-      this.lastCornerX = this.startPosX;
-      this.lastCornerY = this.startPosY;
-    }
-    if (this.direction === "Right") {
-      this.startPosX = 0;
-      this.startPosY = Math.floor(Math.random() * (790 - 10) + 1);
-      this.firstCornerX = this.startPosX;
-      this.firstCornerY = this.startPosY;
-      this.secondCornerX = this.startPosX;
-      this.secondCornerY = this.startPosY;
-      this.lastCornerX = this.startPosX;
-      this.lastCornerY = this.startPosY;
-    }
-    if (this.direction === "Left") {
-      this.startPosX = canvas.width;
-      this.startPosY = Math.floor(Math.random() * (790 - 10) + 1);
-      this.firstCornerX = this.startPosX;
-      this.firstCornerY = this.startPosY;
-      this.secondCornerX = this.startPosX;
-      this.secondCornerY = this.startPosY;
-      this.lastCornerX = this.startPosX;
-      this.lastCornerY = this.startPosY;
-    }
-  }
-
-  draw() {
-    ctx.beginPath();
-
-    ctx.moveTo(this.startPosX, this.startPosY);
-
-    ctx.lineTo(this.firstCornerX, this.firstCornerY);
-
-    ctx.moveTo(this.firstCornerX, this.firstCornerY);
-
-    ctx.lineTo(this.secondCornerX, this.secondCornerY);
-
-    ctx.moveTo(this.secondCornerX, this.secondCornerY);
-
-    ctx.lineTo(this.lastCornerX, this.lastCornerY);
-    ctx.stroke();
-  }
-
-  updateDirection(key) {
-    this._turnCount();
-
-    if (this.direction === "Down") {
-      if (key === "ArrowLeft") {
-        this.direction = "Left";
-      } else if (key === "ArrowRight") {
-        this.direction = "Right";
-      } else if (key === "ArrowUp") {
-        window.alert("You can't turn back");
-      }
-    }
-    if (this.direction === "Right") {
-      if (key === "ArrowUp") {
-        this.direction = "Up";
-      } else if (key === "ArrowDown") {
-        this.direction = "Down";
-      } else if (key === "ArrowLeft") {
-        window.alert("You can't turn back");
-      }
-    }
-    if (this.direction === "Up") {
-      if (key === "ArrowLeft") {
-        this.direction = "Left";
-      } else if (key === "ArrowRight") {
-        this.direction = "Right";
-      } else if (key === "ArrowDown") {
-        window.alert("You can't turn back");
-      }
-    }
-    if (this.direction === "Left") {
-      if (key === "ArrowUp") {
-        this.direction = "Up";
-      } else if (key === "ArrowDown") {
-        this.direction = "Down";
-      } else if (key === "ArrowRight") {
-        window.alert("You can't turn back");
-      }
-    }
-  }
-
-  _turnCount() {
-    this.turnCounter++;
-  }
-  _saveShape() {
-    if (this.turnCounter === null) {
-      shapes.push({
-        startPosX: this.startPosX,
-        startPosY: this.startPosY,
-        lastCornerX: this.lastCornerX,
-        lastCornerY: this.lastCornerY,
-      });
-    } else if (this.turnCounter === 1) {
-      shapes.push({
-        startPosX: this.startPosX,
-        startPosY: this.startPosY,
-        firstCornerX: this.firstCornerX,
-        firstCornerY: this.firstCornerY,
-        lastCornerX: this.lastCornerX,
-        lastCornerY: this.lastCornerY,
-      });
-    } else if (this.turnCounter === 2) {
-      shapes.push({
-        startPosX: this.startPosX,
-        startPosY: this.startPosY,
-        firstCornerX: this.firstCornerX,
-        firstCornerY: this.firstCornerY,
-        secondCornerX: this.secondCornerX,
-        secondCornerY: this.secondCornerY,
-        lastCornerX: this.lastCornerX,
-        lastCornerY: this.lastCornerY,
-      });
-    }
-    console.log(shapes, "turnCounter:", pen.turnCounter);
-  }
-
-  updatePosition() {
-    if (this.turnCounter === null) {
-      switch (this.direction) {
-        case "Down":
-          this.firstCornerY += 1;
-
-          break;
-        case "Right":
-          this.firstCornerX += 1;
-
-          break;
-        case "Up":
-          this.firstCornerY -= 1;
-
-          break;
-        case "Left":
-          this.firstCornerX -= 1;
-
-          break;
-      }
-      this.secondCornerX = this.firstCornerX;
-      this.secondCornerY = this.firstCornerY;
-      this.lastCornerX = this.firstCornerX;
-      this.lastCornerY = this.firstCornerY;
-    } else if (this.turnCounter === 1) {
-      switch (this.direction) {
-        case "Down":
-          this.secondCornerY += 1;
-
-          break;
-        case "Right":
-          this.secondCornerX += 1;
-
-          break;
-        case "Up":
-          this.secondCornerY -= 1;
-
-          break;
-        case "Left":
-          this.secondCornerX -= 1;
-
-          break;
-      }
-      this.lastCornerX = this.secondCornerX;
-      this.lastCornerY = this.secondCornerY;
-    } else if (this.turnCounter === 2) {
-      switch (this.direction) {
-        case "Down":
-          this.lastCornerY += 1;
-
-          break;
-        case "Right":
-          this.lastCornerX += 1;
-
-          break;
-        case "Up":
-          this.lastCornerY -= 1;
-
-          break;
-        case "Left":
-          this.lastCornerX -= 1;
-
-          break;
-      }
-    }
-  }
-}
-
 let pen = new Pen();
+let ball = new Ball();
 
 window.addEventListener("load", () => {
   pen.pickRandomStart();
+  ball._pickRandomStart();
 });
 
 window.addEventListener("keydown", (event) => {
@@ -547,6 +371,9 @@ window.addEventListener("keydown", (event) => {
 
 setInterval(() => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ball._move();
+  ball._draw();
 
   pen.updatePosition();
   pen.draw();
@@ -571,4 +398,5 @@ setInterval(() => {
     }
   }
   drawShapes(shapes);
+  intersectShapes(ball, shapes);
 }, 1000 / 120);
