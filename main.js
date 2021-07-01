3eb1b5889008e7d35c70876326efa53f9bec054f
Simon Nagy <nagysimonmihaly@gmail.com>
2021-07-01 13:26:15 +0200

    add collision checks (not working)

--- main.js
+++ main.js
@@ -9,9 +9,42 @@ let penCurrentPosition = [];
 
 let checkBallLineCollision = () => {
   penCurrentPosition.map((position) => {
-    if (position.x === ball.x && position.y === ball.y) {
+    if (
+      (position.x === ball.x && position.y === ball.y) ||
+      (position.x === ball.x && position.y === ball.y + ball.height) ||
+      (position.x === ball.x + ball.width && position.y === ball.y + ball.width)
+    ) {
       pen.pickRandomStart();
       penCurrentPosition = [];
+    }
+  });
+};
+
+let checkBallWallsCollision = () => {
+  if (ball.x > canvas.width - 20 || ball.x < 0) {
+    ball.xSpeed = -ball.xSpeed;
+  }
+  if (ball.y > canvas.height - 20 || ball.y < 0) {
+    ball.ySpeed = -ball.ySpeed;
+  }
+};
+
+let checkBallShapeCollision = () => {
+  shapes.map((shape) => {
+    let ballLeft = ball.x;
+    let ballRight = ball.x + ball.width;
+    let ballTop = ball.y;
+    let ballBottom = ball.y + ball.height;
+    let shapeLeft = shape.x;
+    let shapeRight = shape.x + shape.width;
+    let shapeTop = shape.y;
+    let shapeBottom = shape.y + shape.height;
+
+    if (ballLeft < shapeRight || ballRight > shapeLeft) {
+      ball.xSpeed = -ball.xSpeed;
+    }
+    if (ballTop > shapeBottom || ballBottom < shapeTop) {
+      ball.ySpeed = -ball.ySpeed;
     }
   });
 };
@@ -30,12 +63,6 @@ class Ball {
     this.y = this.x;
   }
   _move() {
-    if (this.x + this.xSpeed > canvas.width - 20 || this.x + this.xSpeed < 0) {
-      this.xSpeed = -this.xSpeed;
-    }
-    if (this.y + this.ySpeed > canvas.height - 20 || this.y + this.ySpeed < 0) {
-      this.ySpeed = -this.ySpeed;
-    }
     this.x += this.xSpeed;
     this.y += this.ySpeed;
   }
@@ -43,322 +70,10 @@ class Ball {
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
-//check if ball touches line
-let intersectWithLine = (pen, ball) => {
-  //line started on the left
-  if (pen.direction === "left") {
-    //single line
-  }
-};
-//check if ball collides with drawn shapes
-let intersectShapes = (ball, shapes) => {
-  //loop through shapes
-  shapes.forEach((shape) => {
-    //check for shapes with 1 drawn line
-    if (Object.keys(shape).length === 4) {
-      //line started from left, in the upper half of the Canvas
-      if (shape.startPosX === 0 && shape.startPosY < canvas.height / 2) {
-        if (ball.y < shape.startPosY) {
-          ball.ySpeed = -ball.ySpeed;
-        }
-      }
-    }
-  });
-};
 
 let drawShapes = function (shapes) {
   shapes.forEach((shape) => {
-    if (Object.keys(shape).length === 4) {
-      if (shape.startPosX === 0) {
-        if (shape.startPosY > canvas.height / 2) {
-          ctx.fillRect(
-            shape.startPosX,
-            shape.startPosY,
-            canvas.width,
-            canvas.height - shape.startPosY
-          );
-        } else if (shape.startPosY <= canvas.height / 2) {
-          ctx.fillRect(0, 0, canvas.width, shape.startPosY);
-        }
-      } else if (shape.startPosX === canvas.width) {
-        if (shape.startPosY > canvas.height / 2) {
-          ctx.fillRect(
-            shape.lastCornerX,
-            shape.lastCornerY,
-            canvas.width,
-
-            canvas.height - shape.startPosY
-          );
-        } else if (shape.startPosY <= canvas.height / 2) {
-          ctx.fillRect(0, 0, canvas.width, shape.startPosY);
-        }
-      } else if (shape.startPosY === 0) {
-        if (shape.startPosX > canvas.width / 2) {
-          ctx.fillRect(
-            shape.startPosX,
-            shape.startPosY,
-            canvas.width - shape.startPosX,
-            canvas.height
-          );
-        } else if (shape.startPosX <= canvas.width / 2) {
-          ctx.fillRect(0, 0, shape.startPosX, canvas.height);
-        }
-      } else if (shape.startPosY === canvas.height) {
-        if (shape.startPosX > canvas.width / 2) {
-          ctx.fillRect(
-            shape.lastCornerX,
-            shape.lastCornerY,
-            canvas.width - shape.startPosX,
-            canvas.height
-          );
-        } else if (shape.startPosX <= canvas.width / 2) {
-          ctx.fillRect(0, 0, shape.startPosX, canvas.height);
-        }
-      }
-    } else if (Object.keys(shape).length === 6) {
-      if (shape.startPosX === 0) {
-        if (shape.lastCornerY === 0) {
-          ctx.fillRect(0, 0, shape.firstCornerX, shape.firstCornerY);
-        } else if (shape.lastCornerY === canvas.height) {
-          ctx.fillRect(
-            shape.startPosX,
-            shape.startPosY,
-            shape.lastCornerX,
-            shape.lastCornerY
-          );
-        }
-      }
-      if (shape.startPosX === canvas.width) {
-        if (shape.lastCornerY === 0) {
-          ctx.fillRect(
-            shape.lastCornerX,
-            shape.lastCornerY,
-            shape.startPosX,
-            shape.startPosY
-          );
-        } else if (shape.lastCornerY === canvas.height) {
-          ctx.fillRect(
-            shape.firstCornerX,
-            shape.firstCornerY,
-            canvas.width - shape.firstCornerX,
-            canvas.height - shape.startPosY
-          );
-        }
-      }
-      if (shape.startPosY === 0) {
-        if (shape.lastCornerX === 0) {
-          ctx.fillRect(0, 0, shape.startPosX, shape.firstCornerY);
-        } else if (shape.lastCornerX === canvas.width) {
-          ctx.fillRect(
-            shape.startPosX,
-            shape.startPosY,
-            canvas.width - shape.startPosX,
-            shape.lastCornerY
-          );
-        }
-      }
-      if (shape.startPosY === canvas.height) {
-        if (shape.lastCornerX === 0) {
-          ctx.fillRect(
-            shape.lastCornerX,
-            shape.lastCornerY,
-            shape.firstCornerX,
-            canvas.height - shape.firstCornerY
-          );
-        } else if (shape.lastCornerX === canvas.width) {
-          ctx.fillRect(
-            shape.firstCornerX,
-            shape.firstCornerY,
-            canvas.width - shape.firstCornerX,
-            canvas.height - shape.lastCornerY
-          );
-        }
-      }
-    } else if (Object.keys(shape).length === 8) {
-      if (shape.startPosX === 0) {
-        if (shape.lastCornerX === 0 && shape.startPosY > shape.lastCornerY) {
-          ctx.fillRect(
-            shape.lastCornerX,
-            shape.lastCornerY,
-            shape.secondCornerX,
-            shape.startPosY - shape.lastCornerY
-          );
-        } else if (
-          shape.lastCornerX === 0 &&
-          shape.startPosY < shape.lastCornerY
-        ) {
-          ctx.fillRect(
-            shape.startPosX,
-            shape.startPosY,
-            shape.secondCornerX,
-            shape.lastCornerY - shape.startPosY
-          );
-        } else if (
-          shape.startPosX === 0 &&
-          shape.lastCornerX === canvas.width
-        ) {
-          if (shape.startPosY < canvas.height / 2) {
-            ctx.beginPath();
-            ctx.moveTo(shape.startPosX, shape.startPosY);
-            ctx.lineTo(shape.firstCornerX, shape.firstCornerY);
-            ctx.lineTo(shape.secondCornerX, shape.secondCornerY);
-            ctx.lineTo(shape.lastCornerX, shape.lastCornerY);
-            ctx.lineTo(canvas.width, 0);
-            ctx.lineTo(0, 0);
-            ctx.lineTo(shape.startPosX, shape.startPosY);
-            ctx.fill();
-          } else {
-            ctx.beginPath();
-            ctx.moveTo(shape.startPosX, shape.startPosY);
-            ctx.lineTo(shape.firstCornerX, shape.firstCornerY);
-            ctx.lineTo(shape.secondCornerX, shape.secondCornerY);
-            ctx.lineTo(shape.lastCornerX, shape.lastCornerY);
-            ctx.lineTo(canvas.width, canvas.height);
-            ctx.lineTo(0, canvas.height);
-            ctx.lineTo(shape.startPosX, shape.startPosY);
-            ctx.fill();
-          }
-        }
-      } else if (shape.startPosX === canvas.width) {
-        if (
-          shape.lastCornerX === canvas.width &&
-          shape.startPosY > shape.lastCornerY
-        ) {
-          ctx.fillRect(
-            shape.secondCornerX,
-            shape.secondCornerY,
-            canvas.width - shape.secondCornerX,
-            shape.startPosY - shape.lastCornerY
-          );
-        } else if (
-          shape.lastCornerX === canvas.width &&
-          shape.startPosY < shape.lastCornerY
-        ) {
-          ctx.fillRect(
-            shape.firstCornerX,
-            shape.firstCornerY,
-            canvas.width - shape.firstCornerX,
-            shape.lastCornerY - shape.startPosY
-          );
-        } else if (
-          shape.startPosX === canvas.width &&
-          shape.lastCornerX === 0
-        ) {
-          if (shape.startPosY < canvas.height / 2) {
-            ctx.beginPath();
-            ctx.moveTo(shape.startPosX, shape.startPosY);
-            ctx.lineTo(shape.firstCornerX, shape.firstCornerY);
-            ctx.lineTo(shape.secondCornerX, shape.secondCornerY);
-            ctx.lineTo(shape.lastCornerX, shape.lastCornerY);
-            ctx.lineTo(0, 0);
-            ctx.lineTo(canvas.width, 0);
-            ctx.lineTo(shape.startPosX, shape.startPosY);
-            ctx.fill();
-          } else {
-            ctx.beginPath();
-            ctx.moveTo(shape.startPosX, shape.startPosY);
-            ctx.lineTo(shape.firstCornerX, shape.firstCornerY);
-            ctx.lineTo(shape.secondCornerX, shape.secondCornerY);
-            ctx.lineTo(shape.lastCornerX, shape.lastCornerY);
-            ctx.lineTo(0, canvas.height);
-            ctx.lineTo(canvas.width, canvas.height);
-            ctx.lineTo(shape.startPosX, shape.startPosY);
-            ctx.fill();
-          }
-        }
-      } else if (shape.startPosY === 0) {
-        if (shape.lastCornerY === 0 && shape.startPosX < shape.lastCornerX) {
-          ctx.fillRect(
-            shape.startPosX,
-            shape.startPosY,
-            shape.lastCornerX - shape.startPosX,
-            shape.firstCornerY
-          );
-        } else if (
-          shape.lastCornerY === 0 &&
-          shape.startPosX > shape.lastCornerX
-        ) {
-          ctx.fillRect(
-            shape.lastCornerX,
-            shape.lastCornerY,
-            shape.startPosX - shape.lastCornerX,
-            shape.firstCornerY
-          );
-        } else if (
-          shape.startPosY === 0 &&
-          shape.lastCornerY === canvas.height
-        ) {
-          if (shape.startPosX < canvas.width / 2) {
-            ctx.beginPath();
-            ctx.moveTo(shape.startPosX, shape.startPosY);
-            ctx.lineTo(shape.firstCornerX, shape.firstCornerY);
-            ctx.lineTo(shape.secondCornerX, shape.secondCornerY);
-            ctx.lineTo(shape.lastCornerX, shape.lastCornerY);
-            ctx.lineTo(0, canvas.height);
-            ctx.lineTo(0, 0);
-            ctx.lineTo(shape.startPosX, shape.startPosY);
-            ctx.fill();
-          } else {
-            ctx.beginPath();
-            ctx.moveTo(shape.startPosX, shape.startPosY);
-            ctx.lineTo(shape.firstCornerX, shape.firstCornerY);
-            ctx.lineTo(shape.secondCornerX, shape.secondCornerY);
-            ctx.lineTo(shape.lastCornerX, shape.lastCornerY);
-            ctx.lineTo(canvas.width, canvas.height);
-            ctx.lineTo(canvas.width, 0);
-            ctx.lineTo(shape.startPosX, shape.startPosY);
-            ctx.fill();
-          }
-        }
-      } else if (shape.startPosY === canvas.height) {
-        if (
-          shape.lastCornerY === canvas.height &&
-          shape.startPosX < shape.lastCornerX
-        ) {
-          ctx.fillRect(
-            shape.firstCornerX,
-            shape.firstCornerY,
-            shape.lastCornerX - shape.firstCornerX,
-            canvas.height - shape.firstCornerY
-          );
-        } else if (
-          shape.lastCornerY === canvas.height &&
-          shape.startPosX > shape.lastCornerX
-        ) {
-          ctx.fillRect(
-            shape.secondCornerX,
-            shape.secondCornerY,
-            shape.firstCornerX - shape.secondCornerX,
-            canvas.height - shape.firstCornerY
-          );
-        } else if (
-          shape.startPosY === canvas.height &&
-          shape.lastCornerY === 0
-        ) {
-          if (shape.startPosX < canvas.width / 2) {
-            ctx.beginPath();
-            ctx.moveTo(shape.startPosX, shape.startPosY);
-            ctx.lineTo(shape.firstCornerX, shape.firstCornerY);
-            ctx.lineTo(shape.secondCornerX, shape.secondCornerY);
-            ctx.lineTo(shape.lastCornerX, shape.lastCornerY);
-            ctx.lineTo(0, 0);
-            ctx.lineTo(0, canvas.height);
-            ctx.lineTo(shape.startPosX, shape.startPosY);
-            ctx.fill();
-          } else {
-            ctx.beginPath();
-            ctx.moveTo(shape.startPosX, shape.startPosY);
-            ctx.lineTo(shape.firstCornerX, shape.firstCornerY);
-            ctx.lineTo(shape.secondCornerX, shape.secondCornerY);
-            ctx.lineTo(shape.lastCornerX, shape.lastCornerY);
-            ctx.lineTo(canvas.width, 0);
-            ctx.lineTo(canvas.width, canvas.height);
-            ctx.lineTo(shape.startPosX, shape.startPosY);
-            ctx.fill();
-          }
-        }
-      }
-    }
+    ctx.fillRect(shape.x, shape.y, shape.width, shape.height);
   });
 };
 
@@ -410,6 +125,7 @@ setInterval(() => {
     }
   }
   drawShapes(shapes);
-  intersectShapes(ball, shapes);
+  checkBallWallsCollision();
   checkBallLineCollision();
+  checkBallShapeCollision();
 }, 1000 / 120);
--- pen.js
+++ pen.js
@@ -136,34 +136,391 @@ class Pen {
   }
   _saveShape() {
     if (this.turnCounter === null) {
-      shapes.push({
-        startPosX: this.startPosX,
-        startPosY: this.startPosY,
-        lastCornerX: this.lastCornerX,
-        lastCornerY: this.lastCornerY,
-      });
+      if (this.startPosX === 0) {
+        if (this.startPosY > canvas.height / 2) {
+          shapes.push({
+            x: this.startPosX,
+            y: this.startPosY,
+            width: canvas.width,
+            height: canvas.height - this.startPosY,
+          });
+          this.turnCounter = null;
+        } else if (this.startPosY <= canvas.height / 2) {
+          shapes.push({
+            x: 0,
+            y: 0,
+            width: canvas.width,
+            height: this.startPosY,
+          });
+          this.turnCounter = null;
+        }
+      } else if (this.startPosX === canvas.width) {
+        if (this.startPosY > canvas.height / 2) {
+          shapes.push({
+            x: this.lastCornerX,
+            y: this.lastCornerY,
+            width: canvas.width,
+            height: canvas.height - this.startPosY,
+          });
+          this.turnCounter = null;
+        } else if (this.startPosY <= canvas.height / 2) {
+          shapes.push({
+            x: 0,
+            y: 0,
+            width: canvas.width,
+            height: this.startPosY,
+          });
+          this.turnCounter = null;
+        }
+      } else if (this.startPosY === 0) {
+        if (this.startPosX > canvas.width / 2) {
+          shapes.push({
+            x: this.startPosX,
+            y: this.startPosY,
+            width: canvas.width - this.startPosX,
+            height: canvas.height,
+          });
+          this.turnCounter = null;
+        } else if (this.startPosX <= canvas.width / 2) {
+          shapes.push({
+            x: 0,
+            y: 0,
+            width: this.startPosX,
+            height: canvas.height,
+          });
+          this.turnCounter = null;
+        }
+      } else if (this.startPosY === canvas.height) {
+        if (this.startPosX > canvas.width / 2) {
+          shapes.push({
+            x: this.lastCornerX,
+            y: this.lastCornerY,
+            width: canvas.width - this.startPosX,
+            height: canvas.height,
+          });
+          this.turnCounter = null;
+        } else if (this.startPosX <= canvas.width / 2) {
+          shapes.push({
+            x: 0,
+            y: 0,
+            width: this.startPosX,
+            height: canvas.height,
+          });
+          this.turnCounter = null;
+        }
+      }
     } else if (this.turnCounter === 1) {
-      shapes.push({
-        startPosX: this.startPosX,
-        startPosY: this.startPosY,
-        firstCornerX: this.firstCornerX,
-        firstCornerY: this.firstCornerY,
-        lastCornerX: this.lastCornerX,
-        lastCornerY: this.lastCornerY,
-      });
+      if (this.startPosX === 0) {
+        if (this.lastCornerY === 0) {
+          shapes.push({
+            x: 0,
+            y: 0,
+            width: this.firstCornerX,
+            height: this.firstCornerY,
+          });
+          this.turnCounter = null;
+        } else if (this.lastCornerY === canvas.height) {
+          shapes.push({
+            x: this.startPosX,
+            y: this.startPosY,
+            width: this.lastCornerX,
+            height: canvas.height - this.firstCornerY,
+          });
+          this.turnCounter = null;
+        }
+      }
+      if (this.startPosX === canvas.width) {
+        if (this.lastCornerY === 0) {
+          shapes.push({
+            x: this.lastCornerX,
+            y: this.lastCornerY,
+            width: canvas.width - this.firstCornerX,
+            height: this.firstCornerY,
+          });
+          this.turnCounter = null;
+        } else if (this.lastCornerY === canvas.height) {
+          shapes.push({
+            x: this.firstCornerX,
+            y: this.firstCornerY,
+            width: canvas.width - this.firstCornerX,
+            height: this.firstCornerY,
+          });
+          this.turnCounter = null;
+        }
+      }
+      if (this.startPosY === 0) {
+        if (this.lastCornerX === 0) {
+          shapes.push({
+            x: 0,
+            y: 0,
+            width: this.firstCornerX,
+            height: this.firstCornerY,
+          });
+          this.turnCounter = null;
+        } else if (this.lastCornerX === canvas.width) {
+          shapes.push({
+            x: this.startPosX,
+            y: this.startPosY,
+            width: canvas.width - this.startPosX,
+            height: this.lastCornerY,
+          });
+          this.turnCounter = null;
+        }
+      }
+      if (this.startPosY === canvas.height) {
+        if (this.lastCornerX === 0) {
+          shapes.push({
+            x: this.lastCornerX,
+            y: this.lastCornerY,
+            width: this.firstCornerX,
+            height: canvas.height - this.firstCornerY,
+          });
+          this.turnCounter = null;
+        } else if (this.lastCornerX === canvas.width) {
+          shapes.push({
+            x: this.firstCornerX,
+            y: this.firstCornerY,
+            width: canvas.width - this.firstCornerX,
+            height: canvas.height - this.firstCornerY,
+          });
+          this.turnCounter = null;
+        }
+      }
     } else if (this.turnCounter === 2) {
-      shapes.push({
-        startPosX: this.startPosX,
-        startPosY: this.startPosY,
-        firstCornerX: this.firstCornerX,
-        firstCornerY: this.firstCornerY,
-        secondCornerX: this.secondCornerX,
-        secondCornerY: this.secondCornerY,
-        lastCornerX: this.lastCornerX,
-        lastCornerY: this.lastCornerY,
-      });
+      if (this.startPosX === 0) {
+        if (this.lastCornerX === 0 && this.startPosY > this.lastCornerY) {
+          shapes.push({
+            x: this.lastCornerX,
+            y: this.lastCornerY,
+            width: this.secondCornerX,
+            height: this.startPosY - this.lastCornerY,
+          });
+          this.turnCounter = null;
+        } else if (
+          this.lastCornerX === 0 &&
+          this.startPosY < this.lastCornerY
+        ) {
+          shapes.push({
+            x: this.startPosX,
+            y: this.startPosY,
+            width: this.secondCornerX,
+            height: this.lastCornerY - this.startPosY,
+          });
+          this.turnCounter = null;
+        } else if (this.startPosX === 0 && this.lastCornerX === canvas.width) {
+          if (this.startPosY < canvas.height / 2) {
+            shapes.push(
+              {
+                x: 0,
+                y: 0,
+                width: this.firstCornerX,
+                height: this.firstCornerY,
+              },
+              {
+                x: this.firstCornerX,
+                y: 0,
+                width: canvas.width - this.firstCornerX,
+                height: Math.abs(this.secondCornerY - canvas.height),
+              }
+            );
+            this.turnCounter = null;
+          } else {
+            shapes.push(
+              {
+                x: this.startPosX,
+                y: this.startPosY,
+                width: this.firstCornerX,
+                height: canvas.height - this.firstCornerY,
+              },
+              {
+                x: this.secondCornerX,
+                y: this.secondCornerY,
+                width: canvas.width - this.firstCornerX,
+                height: Math.abs(this.secondCornerY - canvas.height),
+              }
+            );
+            this.turnCounter = null;
+          }
+        }
+      } else if (this.startPosX === canvas.width) {
+        if (
+          this.lastCornerX === canvas.width &&
+          this.startPosY > this.lastCornerY
+        ) {
+          shapes.push({
+            x: this.secondCornerX,
+            y: this.secondCornerY,
+            width: canvas.width - this.firstCornerX,
+            height: this.firstCornerY - this.secondCornerY,
+          });
+          this.turnCounter = null;
+        } else if (
+          this.lastCornerX === canvas.width &&
+          this.startPosY < this.lastCornerY
+        ) {
+          shapes.push({
+            x: this.firstCornerX,
+            y: this.firstCornerY,
+            width: canvas.width - this.firstCornerX,
+            height: this.secondCornerY - this.firstCornerY,
+          });
+          this.turnCounter = null;
+        } else if (this.startPosX === canvas.width && this.lastCornerX === 0) {
+          if (shape.startPosY < canvas.height / 2) {
+            shapes.push(
+              {
+                x: 0,
+                y: 0,
+                width: this.firstCornerX,
+                height: this.firstCornerY,
+              },
+              {
+                x: this.firstCornerX,
+                y: this.firstCornerX,
+                width: canvas.width - this.firstCornerX,
+                height: canvas.height - this.secondCornerY,
+              }
+            );
+            this.turnCounter = null;
+          } else {
+            shapes.push(
+              {
+                x: this.lastCornerX,
+                y: this.lastCornerY,
+                width: this.secondCornerX,
+                height: canvas.height - this.secondCornerY,
+              },
+              {
+                x: this.firstCornerX,
+                y: this.firstCornerY,
+                width: canvas.width - this.firstCornerX,
+                height: canvas.height - this.firstCornerY,
+              }
+            );
+            this.turnCounter = null;
+          }
+        }
+      } else if (this.startPosY === 0) {
+        if (this.lastCornerY === 0 && this.startPosX < this.lastCornerX) {
+          shapes.push({
+            x: this.startPosX,
+            y: this.startPosY,
+            width: this.lastCornerX - this.firstCornerX,
+            height: this.firstCornerY,
+          });
+          this.turnCounter = null;
+        } else if (
+          shape.lastCornerY === 0 &&
+          shape.startPosX > shape.lastCornerX
+        ) {
+          shapes.push({
+            x: this.lastCornerX,
+            y: this.lastCornerY,
+            width: this.startPosX - this.lastCornerX,
+            height: this.firstCornerY,
+          });
+          this.turnCounter = null;
+        } else if (
+          shape.startPosY === 0 &&
+          shape.lastCornerY === canvas.height
+        ) {
+          if (shape.startPosX < canvas.width / 2) {
+            shapes.push(
+              {
+                x: 0,
+                y: 0,
+                width: this.startPosX,
+                height: this.firstCornerY,
+              },
+              {
+                x: 0,
+                y: this.firstCornerY,
+                width: this.firstCornerX,
+                height: canvas.height - this.firstCornerY,
+              }
+            );
+            this.turnCounter = null;
+          } else {
+            shapes.push(
+              {
+                x: this.startPosX,
+                y: this.startPosY,
+                width: canvas.width - this.startPosX,
+                height: this.firstCornerY,
+              },
+              {
+                x: this.firstCornerX,
+                y: this.firstCornerX,
+                width: canvas.width - this.firstCornerX,
+                height: canvas.height - this.firstCornerY,
+              }
+            );
+            this.turnCounter = null;
+          }
+        }
+      } else if (this.startPosY === canvas.height) {
+        if (
+          this.lastCornerY === canvas.height &&
+          this.startPosX < this.lastCornerX
+        ) {
+          shapes.push({
+            x: this.firstCornerX,
+            y: this.firstCornerY,
+            width: this.lastCornerX - this.firstCornerX,
+            height: canvas.height - this.firstCornerY,
+          });
+          this.turnCounter = null;
+        } else if (
+          shape.lastCornerY === canvas.height &&
+          shape.startPosX > shape.lastCornerX
+        ) {
+          shapes.push({
+            x: this.secondCornerX,
+            y: this.secondCornerY,
+            width: this.firstCornerX - this.secondCornerX,
+            height: canvas.height - this.firstCornerY,
+          });
+          this.turnCounter = null;
+        } else if (
+          shape.startPosY === canvas.height &&
+          shape.lastCornerY === 0
+        ) {
+          if (shape.startPosX < canvas.width / 2) {
+            shapes.push(
+              {
+                x: 0,
+                y: this.firstCornerY,
+                width: this.firstCornerX,
+                height: canvas.height - this.firstCornerY,
+              },
+              {
+                x: 0,
+                y: 0,
+                width: this.secondCornerX,
+                height: this.secondCornerY,
+              }
+            );
+            this.turnCounter = null;
+          } else {
+            shapes.push(
+              {
+                x: this.firstCornerY,
+                y: this.firstCornerY,
+                width: canvas.width - this.firstCornerX,
+                height: canvas.height - this.firstCornerY,
+              },
+              {
+                x: this.secondCornerX,
+                y: 0,
+                width: canvas.width - this.secondCornerX,
+                height: this.secondCornerY,
+              }
+            );
+            this.turnCounter = null;
+          }
+        }
+      }
     }
-    console.log(shapes, "turnCounter:", pen.turnCounter);
   }
 
   updatePosition() {
